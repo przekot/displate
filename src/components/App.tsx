6015bc4c30ba70b6b1920c51eb27ef80bd0d2644
Michał Brześciński <mbtrzescinski@gog.com>
2019-10-18 12:16:57 +0200

    Basic setup with react, babel, typescript and webpack

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import '../styles/index.css';
+
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				<h1>Displate</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
